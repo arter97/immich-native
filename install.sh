9ce498f4ad97ec4f9d26cdf45d3b88690478eacf
4v3ngR <101933870+4v3ngR@users.noreply.github.com>
2024-06-13 17:34:28 +0800

    Update install.sh
    add support for installing on macOS.
    
    Notes:
    * immich user created as a "share only" user
    * HOME env var is not set for share own user, so we set it
    * add /usr/local/bin to the PATH in the start scripts to allow for node installed with npm

--- install.sh
+++ install.sh
@@ -4,19 +4,35 @@ set -xeuo pipefail
 
 TAG=v1.106.3
 
+UNAME=$(uname)
+
 IMMICH_PATH=/var/lib/immich
+
+if [ "$UNAME" = "Darwin" ]; then
+  # we select a path that is writable and doesn't interfere with brew or other package managers
+  IMMICH_PATH=/opt/services/immich
+fi
+
 APP=$IMMICH_PATH/app
 
 if [[ "$USER" != "immich" ]]; then
-  # Disable systemd services, if installed
-  (
-    for i in immich*.service; do
-      systemctl stop $i && \
-        systemctl disable $i && \
-        rm /etc/systemd/system/$i &&
-        systemctl daemon-reload
+  if [ "$UNAME" = "Darwin" ]; then
+    for i in com.immich*.plist; do
+      [ -f /Library/LaunchDaemons/$i ] && \
+      launchctl unload -w /Library/LaunchDaemons/$i && \
+      rm -f /Library/LaunchDaemons/$i
     done
-  ) || true
+  else
+    # Disable systemd services, if installed
+    (
+      for i in immich*.service; do
+        systemctl stop $i && \
+          systemctl disable $i && \
+          rm /etc/systemd/system/$i &&
+          systemctl daemon-reload
+      done
+    ) || true
+  fi
 
   mkdir -p $IMMICH_PATH
   chown immich:immich $IMMICH_PATH
@@ -40,7 +56,11 @@ rm -rf $IMMICH_PATH/home
 mkdir -p $IMMICH_PATH/home
 echo 'umask 077' > $IMMICH_PATH/home/.bashrc
 
-TMP=/tmp/immich-$(uuidgen)
+if [ "$UNAME" = "Darwin" ]; then
+  export HOME=$IMMICH_PATH/home
+fi
+
+TMP="/tmp/immich-$(uuidgen)"
 git clone https://github.com/immich-app/immich $TMP
 cd $TMP
 git reset --hard $TAG
@@ -104,7 +124,13 @@ wget -o - https://download.geonames.org/export/dump/admin2Codes.txt &
 wget -o - https://download.geonames.org/export/dump/cities500.zip &
 wait
 unzip cities500.zip
-date --iso-8601=seconds | tr -d "\n" > geodata-date.txt
+
+if [ "$UNAME" = "Darwin" ]; then
+  date -Iseconds | tr -d "\n" > geodata-date.txt
+else
+  date --iso-8601=seconds | tr -d "\n" > geodata-date.txt
+fi
+
 rm cities500.zip
 
 # Install sharp
@@ -123,6 +149,8 @@ sed -i -e "s@app.listen(port)@app.listen(port, '127.0.0.1')@g" $APP/dist/main.js
 cat <<EOF > $APP/start.sh
 #!/bin/bash
 
+PATH=\$PATH:/usr/local/bin
+
 set -a
 . $IMMICH_PATH/env
 set +a
@@ -133,6 +161,8 @@ EOF
 
 cat <<EOF > $APP/machine-learning/start.sh
 #!/bin/bash
+
+PATH=\$PATH:/usr/local/bin
 
 set -a
 . $IMMICH_PATH/env
@@ -159,5 +189,9 @@ EOF
 rm -rf $TMP
 
 echo
-echo "Done. Please install the systemd services to start using Immich."
+if [ "$UNAME" = "Darwin" ]; then
+  echo "Done. Please install the LaunchDaemon files in /Library/LaunchDaemons and load to begin using Immich."
+else
+  echo "Done. Please install the systemd services to start using Immich."
+fi
 echo
